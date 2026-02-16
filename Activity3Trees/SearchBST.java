36055c342ea3b8f55361b83f7cbf3a7e742e9c42
riya-rjha <riyaranjanjha2006@gmail.com>
2026-02-16 11:28:43 +0530

    Add SearchBST and TraversalTree classes with basic operations and traversal methods

---
+++ Activity3Trees/SearchBST.java
@@ -0,0 +1,54 @@
+package Activity3Trees;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        this.data = val;
+        this.left = this.right = null;
+    }
+}
+
+public class SearchBST {
+
+    public static boolean search(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == key) {
+            return true;
+        }
+
+        if (root.data < key) {
+            return search(root.right, key);
+        }
+
+        return search(root.left, key);
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            return new Node(val);
+        }
+        if (val < root.data) {
+            root.left = insert(root.left, val);
+        } else if (val > root.data) {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Node root = null;
+        int[] values = {50, 30, 70, 20, 40, 60, 80};
+
+        for (int val : values) {
+            root = insert(root, val);
+        }
+
+        System.out.println("Search 40: " + search(root, 40));
+        System.out.println("Search 100: " + search(root, 100));
+    }
+}
